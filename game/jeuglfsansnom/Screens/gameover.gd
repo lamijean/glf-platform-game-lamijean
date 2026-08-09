c8e7d122cb65ba2417702d61964536a310a6c070
imikado <imikado@gmail.com>
2026-08-09 18:05:30 +0200

    maj

---
+++ game/jeuglfsansnom/Screens/gameover.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var main : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_button_button_up() -> void:
+	get_tree().change_scene_to_packed(main)
+
+	pass # Replace with function body.
